9e9c1a52fb79b4a9edafa54d521150118275d712
The Nithin Balaji <thenithinbalaji@gmail.com>
2022-09-15 01:26:28 +0530

    Update calorie_check.py
    Co-authored-by: Anand <anandarul47@gmail.com>
    Co-authored-by: Anunitha <anunitaprakash103@gmail.com>
    Co-authored-by: Gagan Sai <rollingrocky360@gmail.com>
    Co-authored-by: Sahithi <sahithirrao@gmail.com>
    Co-authored-by: Supraja <supraja2809@gmail.com>

--- calorie_check.py
+++ calorie_check.py
@@ -6,15 +6,10 @@ import json
 from sqlalchemy import create_engine
 
 engine = create_engine("postgresql://postgres:0309@localhost:5432/postgres")
-# conn = psycopg2.connect(host="localhost",database="SIH",user="postgres",password="sup123")
 def calorie_chk(menu, sch_type):
 
-    # cur=conn.cursor()
     df = pd.read_sql_query(
-        "select * from calorie where type='%s'" % (sch_type), con=engine
-    )
-    # temp=pd.read_sql("select * from calorie");
-    # return df
+        "select * from calorie where type='%s'" % (sch_type), con=engine)
     cal = 0
     pro = 0
     for i in range(len(menu)):
@@ -27,6 +22,5 @@ def calorie_chk(menu, sch_type):
     if cal >= 700 and pro >= 20 and sch_type == "Secondary":
         return True
     return False
-
 
 print(calorie_chk([("Pepper Egg", 2), ("Rice", 2)], "Secondary"))
